c69511bfeb3e43315cec34a9a202355f1b498e01
Cesar Barraza <cbarrazaa1@gmail.com>
2020-08-29 16:40:16 -0500

    Init react navigation

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import {enableScreens} from 'react-native-screens';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 
-const App = () => {
+enableScreens();
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Test" component={() => <Text>Hello World!</Text>} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 const styles = StyleSheet.create({});
 
