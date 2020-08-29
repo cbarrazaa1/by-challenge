2a4ff81a22894d23651e54ed06f9520f15a18ef9
Cesar Barraza <cbarrazaa1@gmail.com>
2020-08-29 15:32:00 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
